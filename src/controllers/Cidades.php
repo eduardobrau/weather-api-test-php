3dad32350c609dd386711304320cb26a6ef80e21
eduardobrau <eduardobrau@terra.com.br>
2021-10-31 17:50:08 -0300

    Renomeado metodo index para get_index ja que o metodo sera carregado confome o metodo http da requisicao nao aceitando por exemplo uma listagem de cidades->index() com o uso de PUT neste caso teria que criar um metodo cidades->put_delete() para atender.

--- src/controllers/Cidades.php
+++ src/controllers/Cidades.php
@@ -1,10 +1,23 @@
 <?php
 
+/**
+ * Wheater API Test PHP.
+ *
+ * PHP version 7.4
+ *
+ * @description  	Classe que representa o recurso /cidades e carregar seus metodos conforme a requisição recebida.
+ * @author   			Eduardo Esteves <eduardostevessilva@gmail.com>
+ * @date					31/10/2021 15:30
+ * @link     			https://github.com/eduardobrau/weather-api-test-php
+ */
+
 namespace App\controllers;
 
 
-class Cidades {
-	public function index($params = null) {
+use App\core\Controller;
+
+class Cidades extends Controller {
+	public function get_index($params = null) {
 		echo '{"msg": "Bem vindo a Weather API", "code":200}';
 		die;
 	}
