529ff1ff76224a174f8d2e4ef49991ce9bf02661
eduardobrau <eduardobrau@terra.com.br>
2021-11-18 21:06:05 -0300

    Renomeado alguns metodos para facilitar sua leitura, e finalizado a criacao de testes para o classe Helpers.

--- src/Test/components/HelpersTest.php
+++ src/Test/components/HelpersTest.php
@@ -19,23 +19,99 @@ use PHPUnit\Framework\TestCase;
 
 class HelpersTest extends TestCase {
 
-	public function testshouldReturnJsonStringWhenParametersIsValid() {
+	public function shouldReturnJsonStringWhenParametersIsValid() {
 		$msgJson = Helpers::msgJson('Bad Request', 400);
 		$this->assertEquals('{"msg":"Bad Request","code":400}', $msgJson);
 	}
 
 	/**
-	 * @dataProvider dataProviderMsgToJsonShouldThowException
+	 * @dataProvider msgToJsonShouldThrowExceptionDataProvider
 	*/
-	public function testMsgToJsonShouldThowException($message, $code) {
+	public function testMsgToJsonShouldThrowException($message, $code) {
 		$this->expectException('\Exception');
 		Helpers::msgJson($message, $code);
 	}
 
-	public function dataProviderMsgToJsonShouldThowException() {
+	public function msgToJsonShouldThrowExceptionDataProvider() {
 		return [
 			'shouldThowExceptionWhenMessageIsNotAstring' 	=> ['message' => 1212121, 'code' => 404],
 			'shouldThowExceptionWhenCodeIsNotAnumber' 		=> ['message' => 'Bad Request', 'code' => '404']
+		];
+	}
+
+	/**
+	 * @dataProvider removeLastBarShouldThrowExceptionDataProvider
+	*/
+	public function testRemoveLastBarShouldThrowException($uri) {
+		$this->expectException('\Exception');
+		Helpers::removeLastBar($uri);
+	}
+
+	public function removeLastBarShouldThrowExceptionDataProvider() {
+		return [
+			'shouldThrowExceptionWhenUriIsEmpty' 		 => ['uri' => ''],
+			'shouldThrowExceptionWhenUriIsNotString' => ['uri' => 123123]
+		];
+	}
+
+	/**
+	 * @dataProvider removeLastBarDataProvider
+	*/
+	public function testRemoveLastBar($uri, $expected){
+		$result = Helpers::removeLastBar($uri);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function removeLastBarDataProvider() {
+		return [
+			'shouldRemoveLastBarWhenExist' 		=> ['uri' => 'http://localhost:8000/cidades/', 'expected' => 'http://localhost:8000/cidades'],
+			'shouldNotRemoveWhenHasNotExist' 	=> ['uri' => 'http://localhost:8000/cidades', 'expected' => 'http://localhost:8000/cidades']
+		];
+	}
+
+	/**
+	 * @dataProvider clearJsonDataProvider
+	*/
+	public function testClearJson($json, $expected) {
+		$result = Helpers::clearJson($json);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function clearJsonDataProvider() {
+		return [
+			'shouldClearJsonWhenCotainManySpaces'				  => [
+				'json' => '{"test": "Hello   World!  "}',
+				'expected' => '{"test": "Hello World!"}'
+			],
+			'shouldReturnSameJsonWhenNotCotainManySpaces' => [
+				'json' => '{"test": "Hello World!"}',
+				'expected' => '{"test": "Hello World!"}'
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider jsonToObjectDataProvider
+	*/
+	public function testJsonToObject($json, $expected) {
+		$result = Helpers::jsonToObject($json);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function jsonToObjectDataProvider() {
+		return [
+			'shouldReturnTrueBooleanWhenBeJson' 					=> [
+				'json' => '{"test": "Hello World!"}',
+				'expected' => true
+			],
+			'shouldReturnTrueBooleanWhenBeArrayOfJson' 		=> [
+				'json' => '[{"test": "Hello World!"},{"test2": "Hello World2!"}]',
+				'expected' => true
+			],
+			'shouldReturnFalseBooleanWhenNotBeJson' 			=> [
+				'json' => 'Hello World!',
+				'expected' => false
+			]
 		];
 	}
 
